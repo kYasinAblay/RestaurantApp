4e9413724952a0183ff3814ebf53e5a30bd3c818
kyasinablay <kamilyasinablay@hotmail.com>
2021-11-07 22:15:13 +0300

    finish

--- ArdaPos.DataAccess/SeedData.cs
+++ ArdaPos.DataAccess/SeedData.cs
@@ -12,10 +12,10 @@ namespace ArdaPos.DataAccess
             {
                 var products = new List<Product>()
                 {
-                    new Product { Name = "Pilav", Species="Yemek", Price=21.5m },
-                    new Product { Name = "Kurufasulye", Species="Yemek", Price=12.5m },
-                    new Product { Name = "Salata", Species="Aperatif", Price=12.5m },
-                    new Product { Name = "Sütlaç", Species="Tatlı", Price=25 }
+                    new Product { Name = "Pilav", Species="Yemek", Price=20m },
+                    new Product { Name = "Kuru Fasulye", Species="Yemek", Price=15m },
+                    new Product { Name = "Salata", Species="Aperatif", Price=7.5m },
+                    new Product { Name = "Sütlaç", Species="Tatlı", Price=15 }
                 };
                 context.Products.AddRange(products);
                 context.SaveChanges();
